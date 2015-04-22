653a989814eba1c79f1dba1b900b1f597e2a0626
Cory Forsyth <cory.forsyth@gmail.com>
2015-04-22 19:45:19 -0400

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sample Sessions
+
+Sample app using sessions.
+
+## Install
+
+  * `git clone`
+  * `cd sample-sessions`
+  * `npm install`
+  * `nodemon server.js`
+  * visit http://localhost:5000
--- server.js
+++ server.js
@@ -3,33 +3,22 @@ var app = express();
 var session = require('express-session');
 var port = process.env.PORT || 5000;
 
-app.use(function(req, res, next){
-  console.log('I am middleware!');
-  req.myMiddlewareValue = "I AM MIDDLEWARE";
-  res.blah = true;
-  next();
-});
-
 app.use(session({
   secret: 'something secret!'
 }));
 
-app.get('/index.html', function(req, res){
-  console.log('req.myMiddlewareValue = ', req.myMiddlewareValue);
-  console.log('res.blah: ',res.blah);
-  res.send('I am the index.html from app');
-});
+app.use(express.static('public'));
 
 app.get('/home', function(req, res){
-  if (!req.session.hasBeenHereBefore) {
-    req.session.hasBeenHereBefore = true;
-    res.send('Welcome, you are new!');
+  console.log(req.session);
+  var hasSeen = req.session.hasSeen;
+  if (!hasSeen) {
+    req.session.hasSeen = true;
+    res.send('Welcome, you are new');
   } else {
-    res.send('Welcome back, you');
+    res.send('Welcome back, whoever you are');
   }
 });
-
-app.use(express.static('public'));
 
 app.listen(port, function(){
   console.log('Listening on port: ',port);
